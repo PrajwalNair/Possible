87b90447d21ce67ff4ef6294696903cc19c5326b
Dell <you@example.com>
2022-09-27 12:04:56 +0530

    Servlet 26-09-2022

--- Sudoko/src/main/java/com/xworkz/sudoko/servlet/JobApplicationServlet.java
+++ Sudoko/src/main/java/com/xworkz/sudoko/servlet/JobApplicationServlet.java
@@ -40,8 +40,20 @@ public class JobApplicationServlet extends HttpServlet {
 		String university = req.getParameter("University");
 		String address = req.getParameter("address");
 		String skills = req.getParameter("skills");
-//		String j1 = req.getParameter("skills1");
-//		String j2 = req.getParameter("skills2");
+		String j1 = req.getParameter("skills1");
+		String j2 = req.getParameter("skills2");
+		String j3 = req.getParameter("skills3");
+		String j4 = req.getParameter("skills4");
+		String j5 = req.getParameter("skills5");
+		String j6 = req.getParameter("skills6");
+		String j7 = req.getParameter("skills7");
+		String j8 = req.getParameter("skills8");
+		String j9 = req.getParameter("skills9");
+		String j10 = req.getParameter("skills10");
+		String j11 = req.getParameter("skills11");
+		String j12 = req.getParameter("skills12");
+		String j13 = req.getParameter("skills13");
+		String j14 = req.getParameter("skills14");
 		String salary = req.getParameter("salary");
 		String experience = req.getParameter("experience");
 		String idProof = req.getParameter("idproof");
@@ -57,8 +69,20 @@ public class JobApplicationServlet extends HttpServlet {
 		System.out.println("University :"+university);
 		System.out.println("address :"+address);
 		System.out.println("skills :"+skills);
-//		System.out.println("skills :"+j1);
-//		System.out.println("skills :"+j2);
+		System.out.println(j1);
+		System.out.println(j2);
+		System.out.println(j3);
+		System.out.println(j4);
+		System.out.println(j5);
+		System.out.println(j6);
+		System.out.println(j7);
+		System.out.println(j8);
+		System.out.println(j9);
+		System.out.println(j10);
+		System.out.println(j11);
+		System.out.println(j12);
+		System.out.println(j13);
+		System.out.println(j14);
 		System.out.println("salary :"+salary);
 		System.out.println("exp :"+experience);
 		System.out.println("idproof :"+idProof);
@@ -79,9 +103,22 @@ public class JobApplicationServlet extends HttpServlet {
 		.append("yop :"+yop).append("<br/>")
 		.append("University :"+university).append("<br/>")
 		.append("address :"+address).append("<br/>")
+			
 		.append("skills :"+skills).append("<br/>")
-//		.append("skills :"+j1).append("<br/>")
-//		.append("skills :"+j2).append("<br/>")
+		.append(j1).append("<br/>")
+		.append(j2).append("<br/>")
+		.append(j3).append("<br/>")
+		.append(j4).append("<br/>")
+		.append(j5).append("<br/>")
+		.append(j6).append("<br/>")
+		.append(j7).append("<br/>")
+		.append(j8).append("<br/>")
+		.append(j9).append("<br/>")
+		.append(j10).append("<br/>")
+		.append(j11).append("<br/>")
+		.append(j12).append("<br/>")
+		.append(j13).append("<br/>")
+		.append(j14).append("<br/>")
 		.append("salary :"+salary).append("<br/>")
 		.append("exp :"+experience).append("<br/>")
 		.append("idproof :"+idProof).append("<br/>")
@@ -91,7 +128,8 @@ public class JobApplicationServlet extends HttpServlet {
 		
 		JobApplicationDTO dto = new JobApplicationDTO(name,email,Long.parseLong(phoneNo),
 				Long.parseLong(altPhNo),gender,qualification,Integer.parseInt(yop),university,
-				address,skills,Double.parseDouble(salary),Integer.parseInt(experience),idProof,idProofNo);
+				address,skills,j1,j2,j3,j4,j5,j6,j7,j8,j9,j10,j11,j12,j13,j14,Double.parseDouble(salary),
+				Integer.parseInt(experience),idProof,idProofNo);
 				dtos.add(dto);
 		
 		
